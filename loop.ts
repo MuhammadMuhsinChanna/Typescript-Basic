37b771183d94e0964a05169fe1406c0696eefe6e
Muhammad Muhsin <153229080+MuhammadMuhsinChanna@users.noreply.github.com>
2023-12-13 14:45:18 +0500

    Add files via upload

---
+++ loop.ts
@@ -0,0 +1,25 @@
+ // loops 
+
+ for (let PIAIC = 0; PIAIC < 10 ; PIAIC++) {
+    console.log(`Presidential Initiative Artificial Intelligence & Computing`);
+
+ }
+
+//  // Table in loop 
+  
+for (let N = 1 ; N < 11; N++){
+    let ans = N * 7
+    console.log("7","X", N, "=", ans )
+}
+
+// nested loop
+
+for(let PL = 0; PL < 3 ; PL++){
+    console.log(PL,"Parent Loop" )
+}
+ 
+ for(let CL = 0; CL < 4 ; CL++){
+    console.log(CL, "Child Loop")
+    
+ }
+ 
